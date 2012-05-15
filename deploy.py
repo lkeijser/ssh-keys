d3e6763709ca1600fdbf87d82ee24f310879133e
Léon Keijser <keijser@stone-it.com>
2012-05-15 14:14:46 +0200

    Added GPL header in deploy.py

--- deploy.py
+++ deploy.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
 """
-    Author: L.S. Keijser <leon@gotlinux.nl>
+    This is a small script for user management with ssh-keys
 
-    This script takes care of managing 
-    users on a system. It will create the user,
-    place its SSH keyfile in the homedir and set
-    all permissions and ownership correctly.
+    Copyright (C) 2012 L.S. Keijser <leon@gotlinux.nl>
 
-    It will remove existing users that aren't
-    present in the SSH keys file.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 """
 
 import glob
